900374c6aaaa39d15d0f477e9c169c2e29255517
InFumumVerti <linus.pust@gmail.com>
2022-05-16 20:51:59 +0200

    switch statement extracted.

--- src/fowler/Customer.java
+++ src/fowler/Customer.java
@@ -4,16 +4,21 @@ import java.util.*;
 
 class Customer {
     private String name;
+
     private Vector rentals = new Vector();
+
     public Customer (String newname){
         name = newname;
     };
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
     };
+
     public String getName (){
         return name;
     };
+
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
