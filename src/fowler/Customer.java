7fd3f19342fb870dd8ef088197176250fbc5378e
InFumumVerti <linus.pust@gmail.com>
2022-05-16 21:03:42 +0200

    temp with query

--- src/fowler/Customer.java
+++ src/fowler/Customer.java
@@ -4,13 +4,10 @@ import java.util.*;
 
 class Customer {
     private String name;
-
     private Vector rentals = new Vector();
-
     public Customer (String newname){
         name = newname;
     };
-
     public void addRental(Rental arg) {
         rentals.addElement(arg);
     };
@@ -46,8 +43,25 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental aRental) {
-        return aRental.getCharge();
+    private double amountFor(Rental each) {
+        double thisAmount = 0;
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
+
 }
     
